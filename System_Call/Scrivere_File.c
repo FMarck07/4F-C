06906c3555ff27e9f199f954ae51c27eceb7915d
FMarck07 <150336290+FMarck07@users.noreply.github.com>
2025-01-06 21:54:47 +0100

    Add files via upload

---
+++ System_Call/Scrivere_File.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+
+{
+    if (argc != 2)
+    {
+        printf("Numero argomenti sbagliato\n");
+        exit(1);
+    }
+
+    char alfabeto[] = "abcdefghijklmnopqrstuvz";
+    int fd = open(argv[1], O_WRONLY | O_CREAT, 0644);
+    write(fd, alfabeto, strlen(alfabeto));
+    close(fd);
+
+    return 0;
+}
