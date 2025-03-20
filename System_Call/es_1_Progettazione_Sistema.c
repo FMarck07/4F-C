abdac4b3566323669c64b6181fdbc5287d854716
FMarck07 <150336290+FMarck07@users.noreply.github.com>
2025-03-20 10:04:33 +0200

    Add files via upload

---
+++ System_Call/es_1_Progettazione_Sistema.c
@@ -0,0 +1,39 @@
+/*Scrivere un programma concorrente che realizzi il seguente comando:
+
+cat costituzione.txt | more*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+    if(argc != 2){
+        printf("Errore negli argomenti");
+        return -1;
+    }
+    int pipe1[2];
+    pipe(pipe1);
+    int f = fork();
+    if (f == 0)
+    {
+        close(pipe1[0]); 
+        close(1); //output
+        dup(pipe1[1]);
+        close(pipe1[1]);
+        execl("/bin/cat", "cat", argv[1], (char *)0);
+    }
+    else if(f > 0)
+    {
+        close(pipe1[1]);
+        close(0); //input
+        dup(pipe1[0]);
+        close(pipe1[0]);
+        execl("/bin/more", "more", (char *)0);
+    }
+
+    close(pipe1[0]);
+    close(pipe1[1]);
+    return 0;
+}
